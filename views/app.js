757c56e3dda7483e7be939c29829abd9732e07f5
Christian Lisle <lisleachristian@gmail.com>
2021-07-24 13:58:59 -0500

    Fixed bug where preventDefault was not allowing users to type in chat

--- views/app.js
+++ views/app.js
@@ -430,10 +430,11 @@ function gameStarted()  {
 }
 
 this.document.addEventListener('keydown', function(event) {
-  event.preventDefault(); // Prevent user from scrolling screen with arrow keys
-
+  if (event.key === "arrowLeft" || event.key == "ArrowUp" || event.key == "ArrowRight" || event.key == "ArrowDown") {
+    event.preventDefault(); // Prevent user from scrolling screen with arrow keys
+  }
+  
   if (!event.repeat)  { // event.repeat is true if user is holding down key (this causes issues with server)
-    console.log(event.key);
     if(playerEnabled != -1){ // check to make sure that the game has started
       if (event.key == "ArrowLeft" || event.key == "a") {
         socket.emit('changeDirection', ('left'));
@@ -450,60 +451,3 @@ this.document.addEventListener('keydown', function(event) {
     }
   }
 }, true);
-
-// Mobile input detection (swiping) taken from: // Swipe Detect function taken from: http://www.javascriptkit.com/javatutors/touchevents2.shtml
-window.addEventListener('load', function(){
-    var game = document.getElementById('game');
-    swipedetect(game, function(swipedir){
-        if (swipedir != 'none' && playerEnabled != -1) {
-            socket.emit('changeDirection', (swipedir));
-        }
-    })
-}, false);
-
-function swipedetect(el, callback){
-
-    var touchsurface = el,
-    swipedir,
-    startX,
-    startY,
-    distX,
-    distY,
-    threshold = 150, //required min distance traveled to be considered swipe
-    restraint = 100, // maximum distance allowed at the same time in perpendicular direction
-    allowedTime = 300, // maximum time allowed to travel that distance
-    elapsedTime,
-    startTime,
-    handleswipe = callback || function(swipedir){}
-
-    touchsurface.addEventListener('touchstart', function(e){
-        var touchobj = e.changedTouches[0]
-        swipedir = 'none'
-        dist = 0
-        startX = touchobj.pageX
-        startY = touchobj.pageY
-        startTime = new Date().getTime() // record time when finger first makes contact with surface
-        e.preventDefault()
-    }, false)
-
-    touchsurface.addEventListener('touchmove', function(e){
-        e.preventDefault() // prevent scrolling when inside DIV
-    }, false)
-
-    touchsurface.addEventListener('touchend', function(e){
-        var touchobj = e.changedTouches[0]
-        distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
-        distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
-        elapsedTime = new Date().getTime() - startTime // get time elapsed
-        if (elapsedTime <= allowedTime){ // first condition for awipe met
-            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
-                swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
-            }
-            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
-                swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
-            }
-        }
-        handleswipe(swipedir)
-        e.preventDefault()
-    }, false)
-}
